8cf20d9e9205e0c1f8fe7083203fac830ba90858
Dan Schwartz <schwartz@substantial.com>
2019-01-23 17:12:17 -0800

    Add example function and test

---
+++ app.test.js
@@ -0,0 +1,9 @@
+const { sum } = require("./app");
+
+describe("sum", () => {
+  it("adds two numbers and returns their sum", () => {
+    const result = sum(1, 3);
+    const expectedResult = 4;
+    expect(result).toBe(expectedResult);
+  });
+});
