f8de233f00d5b092464559865d36b6c570a2f2a4
Charlotte Warfel <charlottewarfel@gmail.com>
2019-01-25 17:03:43 -0800

    add isPrime test

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ function stringlength(string) {
   return string.length;
 }
 
+function isPrime(number) {
+  for (var i = 2; i <= number / 2; i++) {
+    console.log(i);
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 module.exports = {
   sum: sum,
-  stringlength: stringlength
+  stringlength: stringlength,
+  isPrime: isPrime
 };
--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-const { sum, stringlength } = require("./app");
+const { sum, stringlength, isPrime } = require("./app");
 
 describe("sum", () => {
   it("adds two numbers and returns their sum", () => {
@@ -14,6 +14,24 @@ describe("stringlength", () => {
   it("checks string lenght and returns a number", () => {
     const result = stringlength("helloworld");
     const expectedResult = 10;
+    expect(result).toBe(expectedResult);
+  });
+});
+
+// isPrime: takes one argument, a number, returns `true` if the number is a prime number and `false` if it isn't.
+
+describe("isPrime", () => {
+  it("checks number to verify if its prime and returns true if its prime", () => {
+    const result = isPrime(11);
+    const expectedResult = true;
+    expect(result).toBe(expectedResult);
+  });
+});
+
+describe("isPrime", () => {
+  it("checks number to verify if its prime and returns false if it is not prime", () => {
+    const result = isPrime(6);
+    const expectedResult = false;
     expect(result).toBe(expectedResult);
   });
 });
